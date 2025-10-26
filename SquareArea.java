1fe6c13d0d4447c569aab8378593b0c21407255e
AChen0000 <143315959+AChen0000@users.noreply.github.com>
2025-10-25 22:47:23 -0400

    Implement SquareArea program to calculate area

---
+++ SquareArea.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SquareArea {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double side; // store the side of the square
+
+        do {
+            // Ask the user to enter the side of the square
+            System.out.print("Enter the side of the square (enter 0 or a negative number to stop): ");
+            side = input.nextDouble();
+
+            // If the side is positive, calculate and print the area
+            if (side > 0) {
+                double area = side * side;
+                System.out.println("The area of the square is: " + area);
+            } 
+            // If the user enters 0 or a negative number, show a goodbye message
+            else {
+                System.out.println("Bye!");
+            }
+
+        } while (side > 0); // Repeat as long as the side is greater than 0
+
+        input.close(); // Close 
+    }
+}
